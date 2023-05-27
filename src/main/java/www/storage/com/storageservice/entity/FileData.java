bf4f2afac6f2a98df148ae639833d30919ab6ffb
Samir Hasanov <samirhasanov18@gmail.com>
2023-05-27 13:02:47 +0400

    created entity class and yaml file to connected postgres

---
+++ src/main/java/www/storage/com/storageservice/entity/FileData.java
@@ -0,0 +1,23 @@
+package www.storage.com.storageservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@Table(name = "file_data",schema = "file")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FileData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String type;
+    private String filePath;
+}
